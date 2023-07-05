63d6e82893be2fec6e1b93065cf596eb97ea34c3
Moritz Warning <moritzwarning@web.de>
2023-07-05 18:00:51 +0200

    ci: use debian 10
    Signed-off-by: Moritz Warning <moritzwarning@web.de>

--- tests/test_spa.py
+++ tests/test_spa.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 
 
 def init_driver():
     driver = webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub",
-        desired_capabilities=DesiredCapabilities.FIREFOX,
+        options=webdriver.FirefoxOptions(),
     )
     return driver
 
