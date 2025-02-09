openwrt/firmware-selector-openwrt-org

commit ec497140595f10a3637333c4418a3680ac7743a0

show upcomming versions below SNAPSHOTS
but don't select them as default. In general a warning message could be
added for these versions.

Signed-off-by: Paul Spooren <mail@aparcar.org>

www/index.js
@@ -831,6 +831,10 @@ async function init() {
         insertSnapshotVersions(versions);
       }
 
+      if (config.upcoming_version != "") {
+        versions.push(obj.upcoming_version);
+      }
+
       return {
         versions: versions,
         image_url_override: obj.image_url_override,

commit 7d5925a66ff059a904f2fce8c310743de8d39d93

index.js: fix showing upcoming versions
config.upcoming_version can be `null` instead of empty string.

Fixes: ec497140595f ("show upcomming versions below SNAPSHOTS")
Signed-off-by: Tianling Shen <cnsztl@gmail.com>

www/index.js
@@ -837,7 +837,7 @@ async function init() {
           }) >= 0
       );
 
-      if (config.upcoming_version != "") {
+      if (config.upcoming_version) {
         versions.push(obj.upcoming_version);
       }
 